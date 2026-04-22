6b4514dc470bb05ebf59f00463b28dc4b2d6deac
PraktikumPMP <praktikumpmp@gmail.com>
2026-04-22 09:43:34 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -10,89 +10,49 @@
 
 #include <stdio.h>
 
-void sum (int x[],int N){
-    int i;
-    int sum = 0;
-    for (i = 0; i <N; i ++){
-        sum = sum + x[i];
+void sumArr(int x[], int N, int *sum) {
+    *sum = 0;
+    for (int i = 0; i < N; i++) {
+        *sum += x[i];
     }
-    printf("SUM %d",sum);
-    return;
 }
 
-void Average (int x[], int N){
-    int i;
-    int sum = 0;
-    for (i = 0; i<N; i++){
-        sum = sum + x[i];
-    }
-    double avg = (float)sum/N;
-    printf("AVG %.2f",avg);
-    return;
+void avgArr(int sum, int N, float *avg) {
+    *avg = (float)sum / N;
 }
 
-void Max (int x[], int N){
-    int i;
-    for (i = 0; i<N-1;i++){
-        for (int j = 0; j <N-i-1; j++){
-            if (x[j]>x[j+1]){
-                int temp = x[j];
-                x[j] = x[j+1];
-                x[j+1] = temp;
-            }
+void maxArr(int x[], int N, int *max, int *idx) {
+    *max = x[0];
+    *idx = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (x[i] > *max) {
+            *max = x[i];
+            *idx = i;
         }
     }
-    printf("MAX %d",x[N-1]);
-    return;
 }
 
-
-int max1 (int x[], int N){
-    int i;
-    for (i = 0; i<N-1;i++){
-        for (int j = 0; j <N-i-1; j++){
-            if (x[j]>x[j+1]){
-                int temp = x[j];
-                x[j] = x[j+1];
-                x[j+1] = temp;
-            }
-        }
-    }
-    return (x[N-1]);
-}
-
-void Idx (int x[],int N, int n){
-    int ketemu =0;
-    for (int i = 0; i<N; i++){
-        if (x[i]==n){
-            if (x[i]= x[i+1]){
-                printf("IDX %d",i-1);
-            }else {
-                printf("IDX %d",i);
-            }
-            ketemu = 1;
-            break;
-        }
-    } if (ketemu == 0){
-        printf("data tidak ada di dalam array\n");
-    }
-    return ;
-}
-
-
-int main(){
+int main() {
     int n;
+    scanf("%d", &n);
+
     int x[n];
-    scanf ("%d",&n);
-    for (int k =0; k<n;k++){
-        scanf("%d",&x[k]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x[i]);
     }
 
-    sum (x,n);
-    Average (x,n);
-    Max (x,n);
-    int a = max1(x,n);
-    Idx (x,n,a);
+    int sum, max, idx;
+    float avg;
+
+    sumArr(x, n, &sum);
+    avgArr(sum, n, &avg);
+    maxArr(x, n, &max, &idx);
+
+    printf("SUM %d\n", sum);
+    printf("AVG %.2f\n", avg);
+    printf("MAX %d\n", max);
+    printf("IDX %d\n", idx);
 
     return 0;
 }
