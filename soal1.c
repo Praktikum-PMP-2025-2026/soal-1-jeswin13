b2d6773f975615bd2c02af32e79133a05014a53f
jeswin13 <jeswindersingh36@gmail.com>
2026-04-22 09:23:19 +0700

    Implement array processing functions in soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,98 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 2 - Data Manipulation and External Files
+ *   Hari dan Tanggal    : Rabu 22-04-2026
+ *   Nama (NIM)          : Jeswinder Singh (13224063)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Pengolahan Array
+ * 
+ */
+
+
 #include <stdio.h>
 
-int main(void) {
+void sum (int x[],int N){
+    int i;
+    int sum = 0;
+    for (i = 0; i <N; i ++){
+        sum = sum + x[i];
+    }
+    printf("SUM %d\n",sum);
+    return;
+}
+
+void Average (int x[], int N){
+    int i;
+    int sum = 0;
+    for (i = 0; i<N; i++){
+        sum = sum + x[i];
+    }
+    double avg = (float)sum/N;
+    printf("AVG %.2f\n",avg);
+    return;
+}
+
+void Max (int x[], int N){
+    int i;
+    for (i = 0; i<N-1;i++){
+        for (int j = 0; j <N-i-1; j++){
+            if (x[j]>x[j+1]){
+                int temp = x[j];
+                x[j] = x[j+1];
+                x[j+1] = temp;
+            }
+        }
+    }
+    printf("MAX %d\n",x[N-1]);
+    return;
+}
+
+
+int max1 (int x[], int N){
+    int i;
+    for (i = 0; i<N-1;i++){
+        for (int j = 0; j <N-i-1; j++){
+            if (x[j]>x[j+1]){
+                int temp = x[j];
+                x[j] = x[j+1];
+                x[j+1] = temp;
+            }
+        }
+    }
+    return (x[N-1]);
+}
+
+void Idx (int x[],int N, int n){
+    int ketemu =0;
+    for (int i = 0; i<N; i++){
+        if (x[i]==n){
+            if (x[i]= x[i+1]){
+                printf("IDX %d\n",i-1);
+            }else {
+                printf("IDX %d\n",i);
+            }
+            ketemu = 1;
+            break;
+        }
+    } if (ketemu == 0){
+        printf("data tidak ada di dalam array\n");
+    }
+    return ;
+}
+
+
+int main(){
+    int n;
+    int x[n];
+    scanf ("%d",&n);
+    for (int k =0; k<n;k++){
+        scanf("%d",&x[k]);
+    }
+
+    sum (x,n);
+    Average (x,n);
+    Max (x,n);
+    int a = max1(x,n);
+    Idx (x,n,a);
+
     return 0;
 }
